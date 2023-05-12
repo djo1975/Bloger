a12039d600b4e535c6f2bd29a2861e6bd206fdb2
djo1975 <mladanilic1975@gmail.com>
2023-05-12 08:23:42 +0200

    Likes model created

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post', foreign_key: 'posts_id'
 
-  after_create :increment_post_likes_counter
+  after_save :update_likes_counter
 
-  def increment_post_likes_counter
-    post.increment_likes_counter
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: :User
-  has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :likes, counter_cache: true
   has_many :comments
 
   validates :title, presence: true, length: { maximum: 250 }
