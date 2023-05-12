b3afe8ec646320a4a46656f0fd7254755900fa22
djo1975 <mladanilic1975@gmail.com>
2023-05-12 16:36:20 +0200

    Added recent_five_comments

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -55,4 +55,27 @@ RSpec.describe Post, type: :model do
       expect(subject).to be_valid
     end
   end
+  describe 'Functionality' do
+    before { user.update(posts_counter: 0) } # reset posts_counter to 0
+
+    context 'create comments' do
+      it 'returns the 5 most recent comments' do
+        5.times do |i|
+          Comment.create(
+            text: "Comment #{i}",
+            post: subject,
+            author: user
+          )
+        end
+
+        expect(subject.recent_five_comments.count).to eq(5)
+      end
+    end
+
+    context 'update posts_counter' do
+      it 'updates the posts_counter of the author' do
+        expect { subject.update_post_counter }.to change { user.posts_counter }.from(0).to(1)
+      end
+    end
+  end
 end
