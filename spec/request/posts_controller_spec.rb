9c5f96291b45ec0ec71968f88b65d2f942fc6159
djo1975 <mladanilic1975@gmail.com>
2023-05-16 15:10:33 +0200

    Used type: :request then type: controller

--- spec/request/posts_controller_spec.rb
+++ spec/request/posts_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe PostsController, type: :request do
   describe 'GET #index' do
     it 'assigns posts for the user' do
       user = User.create(name: 'John Doe')
@@ -8,7 +8,7 @@ RSpec.describe PostsController, type: :controller do
       post1 = Post.create(title: 'Post 1', text: 'Lorem ipsum', author: user)
       post2 = Post.create(title: 'Post 2', text: 'Dolor sit amet', author: user)
 
-      get :index, params: { user_id: user.id }
+      get "/users/#{user.id}/posts"
 
       expect(assigns(:user)).to eq(user)
       expect(assigns(:posts)).to eq([post1, post2])
@@ -17,7 +17,7 @@ RSpec.describe PostsController, type: :controller do
     it 'renders the index template' do
       user = User.create(name: 'John Doe')
 
-      get :index, params: { user_id: user.id }
+      get "/users/#{user.id}/posts"
 
       expect(response).to render_template(:index)
     end
@@ -28,7 +28,7 @@ RSpec.describe PostsController, type: :controller do
       user = User.create(name: 'John Doe')
       post = Post.create(title: 'Post 1', text: 'Lorem ipsum', author: user)
 
-      get :show, params: { user_id: user.id, id: post.id }
+      get "/users/#{user.id}/posts/#{post.id}"
 
       expect(assigns(:user)).to eq(user)
       expect(assigns(:post)).to eq(post)
@@ -40,7 +40,7 @@ RSpec.describe PostsController, type: :controller do
       user = User.create(name: 'John Doe')
       post = Post.create(title: 'Post 1', text: 'Lorem ipsum', author: user)
 
-      get :show, params: { user_id: user.id, id: post.id }
+      get "/users/#{user.id}/posts/#{post.id}"
 
       expect(response).to render_template(:show)
     end
--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe UsersController, type: :request do
   describe 'GET #index' do
     it 'assigns all users to @users' do
       user1 = User.create(name: 'John')
       user2 = User.create(name: 'Jane')
 
-      get :index
+      get '/users'
 
       expect(assigns(:users)).to eq([user1, user2])
     end
 
     it 'renders the index template' do
-      get :index
+      get '/users'
 
       expect(response).to render_template(:index)
     end
@@ -22,7 +22,7 @@ RSpec.describe UsersController, type: :controller do
     it 'assigns the requested user to @user' do
       user = User.create(name: 'John')
 
-      get :show, params: { id: user.id }
+      get "/users/#{user.id}"
 
       expect(assigns(:user)).to eq(user)
     end
@@ -30,7 +30,7 @@ RSpec.describe UsersController, type: :controller do
     it 'renders the show template' do
       user = User.create(name: 'John')
 
-      get :show, params: { id: user.id }
+      get "/users/#{user.id}"
 
       expect(response).to render_template(:show)
     end
